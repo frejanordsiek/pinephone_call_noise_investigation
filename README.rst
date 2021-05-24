e159710b0399f657d1c6da99dfd00fe1c9a5a6e0
Freja Nordsiek <fnordsie@posteo.net>
2021-05-24 17:57:32 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,48 @@
+Overview
+========
+
+During phone calls with poor signal with my PinePhone, the person on the other
+end noticed a loud noise coming through that made it very hard to hear me. On my
+end, I heard a sound coming from the inside of the phone that sounded like my
+past phones when their transmit power was set to high. We managed to get it
+recorded on a recorder and thus the investigation into this sound. My progress
+can be read on the Pine64 Forum
+(https://forum.pine64.org/showthread.php?tid=13922).
+
+The tools here to quantitatively investigate the sound. On Mobian (and likely
+other distros), the sound system is configured during a call in a way that it is
+still possible to record the sound from the builtin microphone while the call is
+going with the standard Sound Recorder app. The sound can be recorded during the
+call in a quite place, exported to a FLAC file by the app, transfered to a
+computer, and converted to a WAV file. The tools here are for analyzing the
+results.
+
+Everything in this repository is licensed under the MIT license
+(https://github.com/frejanordsiek/blob/main/COPYING.txt).
+
+
+Making Spectrograms of The Noise
+================================
+
+:file:`plot_spectrogram.py` is a script for taking a recorded WAV file,
+computing the spectrogram, and exporting the spectrogram to a CSV file
+(optionally gzip compressed) and/or as a plot (any image format supported by
+`matplotlib <https://matplotlib.org>`_).
+
+The ``-c CSVFILE`` option specifies the name of the CSV file to write the data
+to.
+
+The ``-p PLOTFILE`` option specifies the name of the file to write the plot
+to. The extension determines the image format.
+
+The ``-n NPERSEG`` option specifies the number of samples per segment to use
+when computing the spectrogram. The default is ``2**14 = 16384``, which is a
+good value to be able to resolve small frequency peaks while averaging enough
+segments together to get a reasonably smooth spectrogram for a recording of
+about a minute.
+
+The one mandatory argument (the last one) is the path to the sound file to read
+(WAV format).
+
+I am keeping recorded spectrograms in the :file:`spectrograms` (CSV data) and
+:file:`plots` directories. Feel free to add any with a Pull Request.
---
+++ plot_spectrogram.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Freja Nordsiek
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import argparse
+import sys
+
+
+import numpy
+import scipy.io.wavfile
+import scipy.signal
+import matplotlib.pylab as plt
+
+
+__version__ = '0.1'
+
+
+def plot_spectrogram(sound_file, csv_file, plot_file, nperseg):
+    """ Plot the spectogram recorded from the phone's microphone.
+
+    Parameters
+    ----------
+    sound_file : str
+        The path to the sound file. Must be a WAV format.
+    csv_file : str or None
+        The path to write the spectrogram to in CSV form, or ``None`` to
+        not write any. Can be optionally gzip compressed if it ends in a
+        ``'.gz'`` extension.
+    plot_file : str or None
+        The path to write the plot of the spectrogram to, or ``None`` to
+        not generate any plot. Must be an image format supported by
+        ``matplotlib``, which is determined from the extension.
+    nperseg : int
+        The number of samples per segment to use when calculating the
+        spectrogram. Must be positive.
+
+    Raises
+    ------
+    TypeError
+        If an argument has the wrong type.
+    ValueError
+        If an argument has an invalid value.
+    IOError
+        If a file cannot be read or writen.
+
+    """
+    # Check the arguments.
+    if not isinstance(sound_file, str):
+        raise TypeError('sound_file must be str.')
+    if csv_file is not None and not isinstance(csv_file, str):
+        raise TypeError('csv_file must be str or None.')
+    if plot_file is not None and not isinstance(plot_file, str):
+        raise TypeError('plot_file must be str or None.')
+    if not isinstance(nperseg, int):
+        raise TypeError('nperseg must be int.')
+    if nperseg < 1:
+        raise ValueError('nperseg must be positive.')
+    # Read the data and convert to float.
+    sample_frequency, raw_data = scipy.io.wavfile.read(sound_file)
+    data = raw_data.astype('float32')
+
+    # Calculate the spectrogram for each channel and then average the
+    # results across channels.
+    f, pwelch_by_channel = scipy.signal.welch(data, sample_frequency,
+                                              scaling='spectrum',
+                                              nperseg=nperseg, axis=0)
+    if pwelch_by_channel.ndim == 1:
+        pwelch = pwelch_by_channel
+    else:
+        pwelch = pwelch_by_channel.mean(axis=1)
+
+    # Write the spectrogram to the CSV file.
+    if csv_file is not None:
+        numpy.savetxt(csv_file, numpy.vstack((f, pwelch)).T,
+                      fmt='%.9g', delimiter=', ', comments='# ',
+                      header='Frequency (Hz), Power Spectrum (A^2/Hz)')
+
+    # Make the plot. There will be three subplots, each above the other
+    # that plot the spectrum from 0 Hz up to an upper frequency limit
+    # which is different for each one. The upper frequency limits start
+    # from the maximum available and work their way down to smaller and
+    # smaller fequencies.
+    if plot_file is not None:
+        fig, axs = plt.subplots(3, 1, figsize=(7.5, 10),
+                                constrained_layout=True)
+        for i, upper_freq in enumerate((None, 5e3, 500.0)):
+            ax = axs[i]
+            if upper_freq is None:
+                selector = slice(None)
+            else:
+                selector = f <= upper_freq
+            ax.semilogy(f[selector], pwelch[selector], '-k')
+            ax.set_xlabel('Frequency (Hz)')
+            ax.set_ylabel('Power Spectrum (A$^2$ / Hz)')
+            if upper_freq is not None:
+                ax.set_xlim((0, upper_freq))
+            ax.grid('on')
+            ax.tick_params(top=True, right=True)
+        fig.savefig(plot_file, dpi=150, pad_inches=0.0)
+
+
+def _get_parser():
+    """ Get the command line argument parser.
+
+    Returns
+    -------
+    parser : argparse.ArgumentParser
+        The argument parser.
+
+    See Also
+    --------
+    argparse.ArgumentParser
+
+    """
+    parser = argparse.ArgumentParser(
+        description='Plot the spectrogram of the audio recorded from '
+        "the phone's microphone during a test call in order to see "
+        "the noise pickup from the modem's transmitter.")
+
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s ' + str(__version__))
+    parser.add_argument('-c', '--csv', type=str,
+                        default=None,
+                        help='The output file path for the generated '
+                        'spectrogram in numerical form. It will be a '
+                        'CSV file, optionally gzip compressed based '
+                        'on its extension. By default, no file is'
+                        'written.')
+    parser.add_argument('-p', '--plot', type=str,
+                        default=None,
+                        help='The output file path for the generated '
+                        'plot of the spectrogram. It must be an image '
+                        'format supported by matplotlib (based on the '
+                        'extension). By default, no plot is generated.')
+    parser.add_argument('-n', type=int, default=2**14,
+                        help='The number of samples per segment to use '
+                        'when calculating the spectrogram. Must be '
+                        'positve. Larger values increase the fequency '
+                        'resolution but result in less segments to '
+                        'average together (more noise). The default is '
+                        '2**14 = 16384.')
+    parser.add_argument('soundfile', metavar='SOUNDFILE', type=str,
+                        help='The sound file to read. Must be a WAV '
+                        'file.')
+    return parser
+
+
+def main(argv=None):
+    """ Run this program with it connected to the command line.
+
+    Parameters
+    ----------
+    argv : iterable of str, optional
+        The input arguments to use. The default is ``sys.argv``.
+
+    See Also
+    --------
+    sys.argv
+
+    """
+    # Grab the input arguments if none were given.
+    if argv is None:
+        argv = sys.argv
+
+    # Parse the input arguments.
+    parser = _get_parser()
+    args = parser.parse_args(argv[1:])
+
+    # Run.
+    plot_spectrogram(args.soundfile, args.csv, args.plot, args.n)
+
+
+# Run main.
+if __name__ == '__main__':
+    sys.exit(main())
